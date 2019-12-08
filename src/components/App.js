6253309b72b4508204f2a9049de9156200200840
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-12-08 18:38:50 -0500

    user can add products using a form

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
-import '../App.css';
+import "../App.css";
 
-import AddProduct from './AddProduct';
-import ProductsList from './ProductsList';
-import SingleProduct from './SingleProduct';
+import AddProduct from "./AddProduct";
+import ProductsList from "./ProductsList";
+import SingleProduct from "./SingleProduct";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const addProduct = product => {
+    setProducts([...products, product]);
+  };
+
   return (
     <Router>
-      <div>
+      <div id="app">
         <aside>
           <Link to={`/`}>Products</Link>
-          <Link to={`/add-product`}>Add Product</Link>
+          <Link to={`/add-product`}>Add product</Link>
         </aside>
 
         <main>
-          <Route exact path='/' component={ProductsList}></Route>
-          <Route path='/add-product' component={AddProduct}></Route>
-          <Route path='/product/:slug' component={SingleProduct}></Route>
+          <Route exact path="/" component={ProductsList} />
+          <Route
+            path="/add-product"
+            render={({ history }) => (
+              <AddProduct addProduct={addProduct} history={history} />
+            )}
+          />
+          <Route path="/product/:slug" component={SingleProduct} />
         </main>
       </div>
     </Router>
